d837e9173f61c4a5219cfd467c9cdac0bc8adb76
ChristianZibl <145457494+ChristianZibl@users.noreply.github.com>
2025-03-06 16:05:37 +0000

    Update api.update.ts

--- app/routes/api.update.ts
+++ app/routes/api.update.ts
@@ -1,7 +1,7 @@
 import { json } from '@remix-run/node';
 import type { ActionFunction } from '@remix-run/node';
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
 
 const execAsync = promisify(exec);
 
