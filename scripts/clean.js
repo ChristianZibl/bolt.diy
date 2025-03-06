9fb3cc172bab290b773c7eaea7511cd0f02077ee
ChristianZibl <145457494+ChristianZibl@users.noreply.github.com>
2025-03-06 16:03:28 +0000

    Update clean.js

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,6 +1,6 @@
 import { rm, existsSync } from 'fs';
 import { join } from 'path';
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
